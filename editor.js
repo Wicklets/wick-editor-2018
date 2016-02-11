42d38f9ba4f0f86a43c25b26b74bbac61b95fae3
Zach Rispoli <zach.rispoli@gmail.com>
2016-02-11 02:19:38 -0500

    started work on project export

--- editor.js
+++ editor.js
@@ -1,222 +1,267 @@
 $(document).ready(function() {
 
-	// Global editor vars
-	var showUploadAlert = false;
-	var currentWickID = 0;
+    // Global editor vars
+    var showUploadAlert = false;
+    var currentWickID = 0;
 
-	var frames = [];
-	var currentFrame = 1;
-	document.getElementById("frameSelector").value = currentFrame;
+    var frames = [[]];
+    var currentFrame = 1;
+    document.getElementById("frameSelector").value = currentFrame;
 
-	// Setup canvas
-	var canvas = new fabric.Canvas('canvas');
-	canvas.selectionColor = 'rgba(0,0,5,0.1)';
-	canvas.selectionBorderColor = 'grey';
-	canvas.selectionLineWidth = 2;
+    // Setup canvas
+    var canvas = new fabric.Canvas('canvas');
+    canvas.selectionColor = 'rgba(0,0,5,0.1)';
+    canvas.selectionBorderColor = 'grey';
+    canvas.selectionLineWidth = 2;
 
-	var ctx = canvas.getContext('2d');
-
-/*****************************
-	Temporary GUI events
-*****************************/
-
-	$("#prevFrameButton").on("click", function(e){ 
-		prevFrame(); 
-	});
-	$("#nextFrameButton").on("click", function(e){ 
-		nextFrame(); 
-	});
-
-	$("#gotoFrameButton").on("click", function(e){
-		var toFrame = parseInt($('textarea#frameSelector').val());
-		goToFrame(toFrame);
-	});
-
-	$("#deleteObjectButton").on("click", function(e){
-		canvas.getActiveObject().remove();
-	});
-	$("#addScriptButton").on("click", function(e){
-		//alert(canvas.getActiveObject().wickData);
-		canvas.getActiveObject().wickData.clickable = true;
-		canvas.getActiveObject().wickData.toFrame = prompt("Enter a frame:")
-	});
-	$("#exportButton").on("click", function(e){
-		//console.log(JSON.stringify(canvas));
-		//console.log(getSerializedProject());
-		//console.log(canvas.getActiveObject().testBogo);
-		alert("NOT YET IMPLEMENTED");
-	});
-	$("#testActionButton").on("click", function(e){
-		goToFrame(canvas.getActiveObject().wickData.toFrame);
-	});
+    var ctx = canvas.getContext('2d');
 
 /*****************************
-	Mouse events
+    Temporary GUI events
 *****************************/
 
-	// Save mouse coordinates within the canvas.
-	// NOTE: This only works properly when the window is in focus.
-	canvas.on('mouse:move', function(event) {
-		var pointer = canvas.getPointer(event.e);
-		canvas.px = pointer.x;
-		canvas.py = pointer.y;
-	});
+    $("#prevFrameButton").on("click", function(e){ 
+        prevFrame(); 
+    });
+    $("#nextFrameButton").on("click", function(e){ 
+        nextFrame(); 
+    });
+
+    $("#gotoFrameButton").on("click", function(e){
+        var toFrame = parseInt($('textarea#frameSelector').val());
+        goToFrame(toFrame);
+    });
+
+    $("#deleteObjectButton").on("click", function(e){
+        canvas.getActiveObject().remove();
+    });
+    $("#sendToBackButton").on("click", function(e){
+        canvas.sendToBack(canvas.getActiveObject());
+    });
+
+    $("#addScriptButton").on("click", function(e){
+        canvas.getActiveObject().wickData.clickable = true;
+        canvas.getActiveObject().wickData.toFrame = prompt("Enter a frame:")
+    });
+    $("#exportButton").on("click", function(e){
+        storeCurrentFrame();
+        exportProject();
+    });
+    $("#testActionButton").on("click", function(e){
+        goToFrame(canvas.getActiveObject().wickData.toFrame);
+    });
 
 /*****************************
-	Key Events
+    Mouse events
 *****************************/
 
-	var keys = [];
-	var action = false;
-
-	document.body.addEventListener("keydown", function (e) {
-	  keys[e.keyCode] = true;
-		action = true;
-		checkKeys();
-	});
-
-	document.body.addEventListener("keyup", function (e) {
-	  keys[e.keyCode] = false;
-		action = false;
-	});
-
-	function checkKeys() {
-		if (keys[16]) { // Shift
-			if (keys[39]) { // Right arrow
-				nextFrame();
-			} else if (keys[37]) { // Left arrow
-				prevFrame();
-			}
-		}
-	}
+    // Save mouse coordinates within the canvas.
+    // NOTE: This only works properly when the window is in focus.
+    canvas.on('mouse:move', function(event) {
+        var pointer = canvas.getPointer(event.e);
+        canvas.px = pointer.x;
+        canvas.py = pointer.y;
+    });
 
 /*****************************
-	Drag and drop events
+    Key Events
 *****************************/
 
-	$("#canvasContainer").on('dragover', function(e) {
-		showUploadAlert = true;
-		return false;
-	});
-	$("#canvasContainer").on('dragleave', function(e) {
-		showUploadAlert = false;
-		return false;
-	});
-	$("#canvasContainer").on('drop', function(e) {
-		// prevent browser from opening the file
-		e.stopPropagation();
-		e.preventDefault();
+    var keys = [];
+    var action = false;
 
-		// retrieve uploaded files data
-		// TODO: multiple files at once
-		var files = e.originalEvent.dataTransfer.files;
-		var file = files[0];
+    document.body.addEventListener("keydown", function (e) {
+      keys[e.keyCode] = true;
+        action = true;
+        checkKeys();
+    });
 
-		// read file as data URL
-		var reader = new FileReader();
-		reader.onload = (function(theFile) {
-			return function(e) {
-				fabric.Image.fromURL(e.target.result, function(oImg) {
-					// add new object to fabric canvas
-					oImg.left = (canvas.width/2) - (oImg.width/2);
-					oImg.top = (canvas.height/2) - (oImg.height/2);
+    document.body.addEventListener("keyup", function (e) {
+      keys[e.keyCode] = false;
+        action = false;
+    });
 
-					oImg.wickData = {}
-					oImg.wickData.clickable = false;
-
-					canvas.add(oImg);
-				});
-			};
-		})(file);
-		reader.readAsDataURL(file);
-
-		showUploadAlert = false;
-
-		return false;
-	});
+    function checkKeys() {
+        if (keys[16]) { // Shift
+            if (keys[39]) { // Right arrow
+                nextFrame();
+            } else if (keys[37]) { // Left arrow
+                prevFrame();
+            }
+        }
+    }
 
 /*****************************
-	Timeline
+    Drag and drop events
 *****************************/
 
-	// Load serialized frames
-	function storeCurrentFrame() {
-		frames[currentFrame] = [];
-		canvas.forEachObject(function(obj){
-		    frames[currentFrame].push(obj);
-		});
-	}
+    $("#canvasContainer").on('dragover', function(e) {
+        showUploadAlert = true;
+        return false;
+    });
+    $("#canvasContainer").on('dragleave', function(e) {
+        showUploadAlert = false;
+        return false;
+    });
+    $("#canvasContainer").on('drop', function(e) {
+        // prevent browser from opening the file
+        e.stopPropagation();
+        e.preventDefault();
 
-	// Save serialized frames
-	function loadFrame(frame) {
-		if (frames[frame] === undefined) {
-			// We're in a frame that doesn't exist. Just draw a blank canvas.
-			canvas.clear();
-		} else {
-			// Load the JSON string and immediately use canvas.renderAll as a callback
-			//canvas.loadFromJSON(frames[frame], canvas.renderAll.bind(canvas));
-			canvas.clear();
-			for(var i = 0; i < frames[frame].length; i++) {
-				canvas.add(frames[frame][i]);
-			}
-		}
-	}
+        // retrieve uploaded files data
+        // TODO: multiple files at once
+        var files = e.originalEvent.dataTransfer.files;
+        var file = files[0];
 
-	// Goes to a specified frame
-	function goToFrame(toFrame) {
-		storeCurrentFrame();
+        // read file as data URL
+        var reader = new FileReader();
+        reader.onload = (function(theFile) {
+            return function(e) {
+                fabric.Image.fromURL(e.target.result, function(oImg) {
+                    // add new object to fabric canvas
+                    oImg.left = (canvas.width/2) - (oImg.width/2);
+                    oImg.top = (canvas.height/2) - (oImg.height/2);
 
-		currentFrame = toFrame;
-		loadFrame(currentFrame);
+                    oImg.wickData = {}
+                    oImg.wickData.clickable = false;
 
-		document.getElementById("frameSelector").value = currentFrame;
-	}
+                    canvas.add(oImg);
+                });
+            };
+        })(file);
+        reader.readAsDataURL(file);
 
-	// Go to the next frame.
-	function nextFrame() {
-		goToFrame(currentFrame + 1);
-	}
+        showUploadAlert = false;
 
-	// Go to the previous frame.
-	function prevFrame() {
-		var toFrame = currentFrame - 1;
-		if (toFrame > 0) {
-			goToFrame(toFrame);
-		}
-	}
+        return false;
+    });
 
 /*****************************
-	Draw loop
+    Timeline
 *****************************/
 
-	// update canvas size on window resize
-	window.addEventListener('resize', resizeCanvas, false);
-	function resizeCanvas() {
-		// for raw html5 canvas
-		//canvas.width = window.innerWidth;
-		//canvas.height = window.innerHeight;
+    // Load serialized frames
+    function storeCurrentFrame() {
+        frames[currentFrame] = [];
+        canvas.forEachObject(function(obj){
+            frames[currentFrame].push(obj);
+        });
+    }
 
-		// for fabric.js canvas
-		canvas.setWidth( window.innerWidth );
-		canvas.setHeight( window.innerHeight );
+    // Save serialized frames
+    function loadFrame(frame) {
+        if (frames[frame] === undefined) {
+            // We're in a frame that doesn't exist. Just draw a blank canvas.
+            canvas.clear();
+        } else {
+            // Load the JSON string and immediately use canvas.renderAll as a callback
+            //canvas.loadFromJSON(frames[frame], canvas.renderAll.bind(canvas));
+            canvas.clear();
+            for(var i = 0; i < frames[frame].length; i++) {
+                canvas.add(frames[frame][i]);
+            }
+        }
+    }
 
-		canvas.calcOffset();
-	}
-	resizeCanvas();
+    // Goes to a specified frame
+    function goToFrame(toFrame) {
+        storeCurrentFrame();
 
-	// start draw/update loop
-	var FPS = 30;
-	setInterval(function() {
-		draw();
-	}, 1000/FPS);
+        currentFrame = toFrame;
+        loadFrame(currentFrame);
 
-	function draw() {
-		if(showUploadAlert) {
-			ctx.fillStyle = '#000000';
-			ctx.textAlign = 'center';
-			ctx.font = "30px Arial";
-			ctx.fillText("Drop image to add to scene...",
-			              canvas.width/2,canvas.height/2);
-		}
-	}
+        document.getElementById("frameSelector").value = currentFrame;
+    }
+
+    // Go to the next frame.
+    function nextFrame() {
+        goToFrame(currentFrame + 1);
+    }
+
+    // Go to the previous frame.
+    function prevFrame() {
+        var toFrame = currentFrame - 1;
+        if (toFrame > 0) {
+            goToFrame(toFrame);
+        }
+    }
+
+/*****************************
+    Draw loop
+*****************************/
+
+    // update canvas size on window resize
+    window.addEventListener('resize', resizeCanvas, false);
+    function resizeCanvas() {
+        // for raw html5 canvas
+        //canvas.width = window.innerWidth;
+        //canvas.height = window.innerHeight;
+
+        // for fabric.js canvas
+        canvas.setWidth( window.innerWidth );
+        canvas.setHeight( window.innerHeight );
+
+        canvas.calcOffset();
+    }
+    resizeCanvas();
+
+    // start draw/update loop
+    var FPS = 30;
+    setInterval(function() {
+        draw();
+    }, 1000/FPS);
+
+    function draw() {
+        if(showUploadAlert) {
+            ctx.fillStyle = '#000000';
+            ctx.textAlign = 'center';
+            ctx.font = "30px Arial";
+            ctx.fillText("Drop image to add to scene...",
+                          canvas.width/2,canvas.height/2);
+        }
+    }
+
+/*****************************
+    Import/Export
+*****************************/
+
+    function exportProject() {
+        wickObjectFrames = [];
+        for(var fi = 0; fi < frames.length; fi++) {
+            var frame = frames[fi];
+            wickObjectFrames[fi] = [];
+            for(var i = 0; i < frame.length; i++) {
+                var obj = frame[i];
+                wickObj = fabricObjectToWickObject(obj)
+                console.log(wickObj);
+                wickObjectFrames[fi].push(wickObj);
+            }
+        }
+    }
+
+/**********************************
+  Object constructors & definitons
+***********************************/
+
+    function fabricObjectToWickObject(fabObj) {
+        obj = wickObject();
+
+        obj.left    = fabObj.left;
+        obj.top     = fabObj.top;
+        obj.width   = fabObj.width;
+        obj.height  = fabObj.height;
+        obj.scaleX  = fabObj.scaleX;
+        obj.scaleY  = fabObj.scaleY;
+        obj.angle   = fabObj.angle;
+        obj.flipX   = fabObj.flipX;
+        obj.flipY   = fabObj.flipY;
+        obj.opacity = fabObj.opacity;
+        obj.src     = fabObj.src;
+
+        return obj;
+    }
+
+    function wickObject() {
+        return {};
+    }
 })
