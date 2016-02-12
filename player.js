52a8c7ed3579c8ae29d3f60895842b413f788238
Zach Rispoli <zach.rispoli@gmail.com>
2016-02-12 02:12:40 -0500

    more work on player

--- player.js
+++ player.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var frames = [[]];
     var currentFrame = 1;
     var projectLoaded = false;
+    var mousePos;
 
     // Setup canvas
     var canvas = document.getElementById("canvas");
@@ -15,14 +16,46 @@ $(document).ready(function() {
     
     var fileInput = document.getElementById('projectFile');
     fileInput.addEventListener('change', function(e) {
+
         var file = fileInput.files[0];
         var reader = new FileReader();
         reader.onload = function(e) {
+            // load project JSON
             frames = JSON.parse(reader.result);
             projectLoaded = true;
+
+            // make canvas images out of src
+            for(var f = 0; f < frames.length; f++) {
+                var frame = frames[f];
+                for(var i = 0; i < frame.length; i++) {
+                    var obj = frame[i];
+                    obj.image = new Image();
+                    obj.image.src = obj.src;
+                }
+            }
         }
         reader.readAsText(file);
     });
+
+/*****************************
+    Mouse events
+*****************************/
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
+
+    canvas.addEventListener('mousemove', function(evt) {
+        mousePos = getMousePos(canvas, evt);
+    }, false);
+
+    document.getElementById("canvasContainer").addEventListener("mousedown", function(event) {
+        // TODO: Check for clickable wickObjects in current frame
+    }, false);
 
 /*****************************
     Draw loop
@@ -39,20 +72,25 @@ $(document).ready(function() {
     // start draw/update loop
     var FPS = 30;
     setInterval(function() {
+        update();
         draw();
     }, 1000/FPS);
 
+    function update() {
+        
+    }
+
     function draw() {
+        // Clear canvas
         context.fillStyle = '#FFFFFF';
         context.rect(0, 0, canvas.width, canvas.height);
         context.fill();
 
+        // Draw current frame content
         if(projectLoaded) {
-            context.fillStyle = '#000000';
-            context.textAlign = 'center';
-            context.font = "12px Arial";
             for(var i = 0; i < frames[currentFrame].length; i++) {
-                context.fillText("Nothing.",canvas.width/2,canvas.height/2+i*12);
+                var obj = frames[currentFrame][i];
+                context.drawImage(obj.image, obj.left, obj.top);
             }
         }
     }
