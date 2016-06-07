6aa2f4977f46cb9e85b58d0b4b5e6d0b3415c891
Zach Rispoli <zach.rispoli@gmail.com>
2016-06-07 18:56:39 -0400

    cleanup

--- src/util.js
+++ src/util.js
@@ -17,6 +17,43 @@
 		return fileString;
 	}
 
+	wickUtils.saveProjectAsHTMLFile = function (projectJSON) {
+		var fileOut = "";
+
+		// Add the player webpage (need to download the empty player)
+		fileOut += WickUtils.downloadFile("player.htm") + "\n";
+
+		// Add the any libs that the player needs
+		fileOut += "<script>" + WickUtils.downloadFile("lib/fpscounter.js") + "</script>\n";
+
+		// Add the player (need to download the player code)
+		fileOut += "<script>" + WickUtils.downloadFile("src/player.js") + "</script>\n";
+
+		// Bundle the JSON project
+		fileOut += "<script>WickPlayer.runProject('" + project + "');</script>" + "\n";
+
+		// Save whole thing as html file
+		var blob = new Blob([fileOut], {type: "text/plain;charset=utf-8"});
+		saveAs(blob, "project.html");
+	}
+
+	wickUtils.saveProjectAsJSONFile = function (projectJSON) {
+		// Save JSON project and have user download it
+		var blob = new Blob([projectJSON], {type: "text/plain;charset=utf-8"});
+		saveAs(blob, "project.json");
+	}
+
+	wickUtils.readJSONFromFileChooser = function (filePath, callback) {
+		if(filePath.files && filePath.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				jsonString = e.target.result;
+				callback(jsonString);
+			};
+			reader.readAsText(filePath.files[0]);
+		}
+	}
+
 	return wickUtils;
 
 })();
