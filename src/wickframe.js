f31ba544c859772c94dbead1838bc66915c66c59
Luxapodular <luca.damasco@gmail.com>
2016-06-18 20:36:50 -0700

    Create tween object definition.

--- src/wickframe.js
+++ src/wickframe.js
@@ -4,7 +4,21 @@
 
 var WickFrame = function () {
 
+	// Store all objects in frame. 
 	this.wickObjects = [];
+
+	// Should we stop on this frame? 
 	this.breakpoint = false;
 
+	// Create multiframe support. 
+	this.frameLength = 1;
+	this.tweenPoints = [];
+
+	var extend = function(length) {
+		this.frameLength += length; 
+	}
+
+	var shrink = function(length) {
+		this.frameLength -= length; 
+	}
 };
---
+++ src/wicktweenobject.js
@@ -0,0 +1,24 @@
+/*****************************
+	   Tween Objects
+*****************************/
+
+/*
+ * Defines a tween object class which stores all possible
+ * manipulations that can be done on a series of wickObjects. 
+ * Tween objects can also recieve wickObjects and apply these 
+ * transformations to the objects themselves. */ 
+
+var wickTween = function() {
+	this.x = 0; 
+	this.y = 0; 
+	this.z = 0; 
+
+	this.scale = 1; 
+	this.theta = 0; 
+
+	this.opacity = 1; //  0 to 1 
+
+	// Default to linear interpolation. 
+	this.interpolationStyle = "LINEAR"; 
+}
+
