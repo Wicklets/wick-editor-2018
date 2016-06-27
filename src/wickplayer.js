d28925e4e4f5367c4860ea94867f02083076c075
Zach Rispoli <zach.rispoli@gmail.com>
2016-06-27 16:38:15 -0400

    text works with new renderer

--- src/wickplayer.js
+++ src/wickplayer.js
@@ -349,16 +349,16 @@ var WickPlayer = (function () {
 				subObj.pixiSprite.interactive = true;
 				subObj.pixiSprite.on('mousedown', onDown);
 				subObj.pixiSprite.on('touchstart', onDown);
-			} else if (subObj) {
+			} else if (subObj.fontData) {
 				var style = {
-					font : 'bold italic 36px Arial',
-					fill : '#F7EDCA',
-					stroke : '#4a1850',
-					strokeThickness : 5,
-					dropShadow : true,
-					dropShadowColor : '#000000',
-					dropShadowAngle : Math.PI / 6,
-					dropShadowDistance : 6,
+					font : "normal " + subObj.fontData.fontSize + "px " + subObj.fontData.fontFamily,
+					fill : subObj.fontData.fill,
+					//stroke : '#4a1850',
+					//strokeThickness : 5,
+					//dropShadow : true,
+					//dropShadowColor : '#000000',
+					//dropShadowAngle : Math.PI / 6,
+					//dropShadowDistance : 6,
 					wordWrap : true,
 					wordWrapWidth : 440
 				};
