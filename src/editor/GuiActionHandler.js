5fc3aaaeaa92d5537bb2b570c989158506f442dc
joshrispoli <joshrispoli@gmail.com>
2016-10-10 00:12:38 -0400

    Move browser copy/paste to GuiActionHandler

--- src/editor/GuiActionHandler.js
+++ src/editor/GuiActionHandler.js
@@ -87,12 +87,11 @@ var GuiActionHandler = function (wickEditor) {
         });
     };
 
+    /*************************
+        Key listeners
+    *************************/
     document.body.addEventListener("keydown", function (event) {
-        /* Set event keyCode in keys dictionary to true. */
         that.keys[event.keyCode] = true;
-
-        /* Determine controlKeyDown and shiftKeyDown. */
-        var controlKeyDown = that.keys[91] || that.keys[224]; // ?
 
         guiActions.forEach(function(guiAction) {
 
@@ -126,7 +125,9 @@ var GuiActionHandler = function (wickEditor) {
         that.keys[event.keyCode] = false;
     });
 
-    /* GuiAction definitions. */
+    /****************************
+        GuiAction Definitions
+    *****************************/
 
     // Space
     // Open Pan Tool
@@ -243,5 +244,86 @@ var GuiActionHandler = function (wickEditor) {
     // Stop Running Project
     new GuiAction([], ['closeBuiltinPlayerButton'], {}, function(args) {
         wickEditor.builtinPlayerInterface.stopRunningProject();
+    });
+
+/*************************
+    Browser Copy/Paste
+*************************/
+
+    // In order to ensure that the browser will fire clipboard events, we always need to have something selected
+    var focusHiddenArea = function () {
+        if($("#scriptingGUI").css('visibility') === 'hidden') {
+            $("#hidden-input").val(' ');
+            $("#hidden-input").focus().select();
+        }
+    }
+
+    document.addEventListener("copy", function(event) {
+
+        that.clearKeys();
+
+        // Don't try to copy from the fabric canvas if user is editing text
+        if(document.activeElement.nodeName == 'TEXTAREA' || wickEditor.interfaces['scriptingide'].scriptingIDEopen) {
+            return;
+        }
+
+        event.preventDefault();
+        
+        // Make sure an element is focused so that copy event fires properly
+        //focusHiddenArea();
+
+        // Generate clipboard data
+        var ids = wickEditor.interfaces['fabric'].getSelectedObjectIDs();
+        var objectJSONs = [];
+        for(var i = 0; i < ids.length; i++) {
+            objectJSONs.push(wickEditor.project.getObjectByID(ids[i]).getAsJSON());
+        }
+        var clipboardObject = {
+            /*position: {top  : group.top  + group.height/2, 
+                       left : group.left + group.width/2},*/
+            groupPosition: {x : 0, 
+                            y : 0},
+            wickObjectArray: objectJSONs
+        }
+        var copyData =  JSON.stringify(clipboardObject);
+
+        // Send the clipboard data to the clipboard!
+        event.clipboardData.setData('text/wickobjectsjson', copyData);
+    });
+
+    document.addEventListener("cut", function(event) {
+        console.error('cut NYI');
+    });
+
+    document.addEventListener("paste", function(event) {
+
+        that.clearKeys();
+
+        if(document.activeElement.nodeName === 'TEXTAREA' || wickEditor.interfaces['scriptingide'].scriptingIDEopen) {
+            return;
+        }
+
+        event.preventDefault();
+        //focusHiddenArea();
+
+        var clipboardData = event.clipboardData;
+        var items = clipboardData.items || clipboardData.types;
+
+        for (i=0; i<items.length; i++) {
+
+            var fileType = items[i].type || items[i];
+            var file = clipboardData.getData(fileType);
+
+            if(fileType === 'text/wickobjectsjson') {
+                var fileWickObject = WickObject.fromJSONArray(JSON.parse(file), function(objs) {
+                    wickEditor.actionHandler.doAction('addObjects', {wickObjects:objs});
+                });
+            } else if (fileType === 'text/plain') {
+                wickEditor.actionHandler.doAction('addObjects', {wickObjects:[WickObject.fromText(file)]});
+            } else {
+                console.error("Pasting files with type " + fileType + "NYI.")
+            }
+
+        }
     });
 }
