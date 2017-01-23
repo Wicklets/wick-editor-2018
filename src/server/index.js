50fc8f1cf57f1fa638c819f8406d178b6d0d9698
Zach Rispoli <zach.rispoli@gmail.com>
2017-01-23 18:55:59 -0500

    add server stuff for real (for real)

---
+++ src/server/index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+  	console.log(msg)
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
