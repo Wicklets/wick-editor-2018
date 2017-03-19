44eba4db9df282dbe00bc936e70468791d136497
Luxapodular <luca.damasco@gmail.com>
2017-03-19 15:31:30 -0400

    Updating Copyright and License Information. FR33 SOFTWAR3 B)

--- src/editor/Tools.Dropper.js
+++ src/editor/Tools.Dropper.js
@@ -1,5 +1,20 @@
-/* Wick - (c) 2016 Zach Rispoli, Luca Damasco, and Josh Rispoli */
+/* Wick - (c) 2017 Zach Rispoli, Luca Damasco, and Josh Rispoli */
 
+/*  This file is part of Wick. 
+    
+    Wick is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wick is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Wick.  If not, see <http://www.gnu.org/licenses/>. */
+    
 if(!window.Tools) Tools = {};
 
 Tools.Dropper = function (wickEditor) {
