3533ad67fd322abcac6d827c0037ae0e45485901
Zach Rispoli <zach.rispoli@gmail.com>
2017-07-28 16:54:59 -0400

    tryin to quickfix fill bucket issues

--- src/editor/Tools.Pen.js
+++ src/editor/Tools.Pen.js
@@ -68,7 +68,7 @@ Tools.Pen = function (wickEditor) {
                 if(hole) {
                     hole.fillColor = wickEditor.settings.fillColor;
                     hole.strokeColor = wickEditor.settings.fillColor;
-                    hole.strokeWidth = 1;
+                    hole.strokeWidth = 3;
                     var superPathString = hole.exportSVG({asString:true});
                     var svgString = '<svg id="svg" version="1.1" width="'+hole.bounds._width+'" height="'+hole.bounds._height+'" xmlns="http://www.w3.org/2000/svg">' +superPathString+ '</svg>'
                     var superPathWickObject = WickObject.createPathObject(svgString);
