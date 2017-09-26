5d84d1ab1e41b2d5e1f927ae0644c049a80040dd
Zach Rispoli <zach.rispoli@gmail.com>
2017-09-26 17:47:29 -0400

    stroke width/brush size previews

--- src/editor/Interfaces.Toolbar.js
+++ src/editor/Interfaces.Toolbar.js
@@ -24,19 +24,70 @@ var ToolbarInterface = function (wickEditor) {
 
     var colorPickerDivs = [];
 
+    var brushSizePreview;
+
     this.setup = function () {
         toolbarDiv = document.getElementById('tools');
 
         // Build tool button elements
-
         for(toolName in wickEditor.tools) {
-            createToolButtonDiv(toolName);
+            var toolButton = new ToolButton(toolName)
+            toolbarDiv.appendChild(toolButton);
         }
 
         toolbarDiv.appendChild(document.createElement('hr'));
 
-        createColorPickerDiv('fillColor', 'toolbar-fill-color');
-        createColorPickerDiv('strokeColor', 'toolbar-fill-color');
+        // Build color pickers
+        var fillColorPicker = new ColorPicker('fillColor', 'toolbar-fill-color');
+        colorPickerDivs.push(fillColorPicker);
+        toolbarDiv.appendChild(fillColorPicker);
+
+        var strokeColorPicker = new ColorPicker('strokeColor', 'toolbar-stroke-color');
+        colorPickerDivs.push(strokeColorPicker);
+        toolbarDiv.appendChild(strokeColorPicker);
+
+        brushSizePreview = new BrushSizePreview();
+        toolbarDiv.appendChild(brushSizePreview);
+        brushSizePreview.refresh();
+
+        var numberInput = new SlideyNumberInput({
+            onsoftchange: function (e) {
+                brushSizePreview.refresh(parseFloat(e));
+            },
+            onhardchange: function (e) {
+                wickEditor.settings.setValue('brushThickness', parseInt(e));
+                wickEditor.syncInterfaces();
+            },
+            min: 1,
+            max: 30,
+            moveFactor: 0.2,
+            initValue: wickEditor.settings.brushThickness,
+        });
+        numberInput.className = 'toolbar-number-input';
+        toolbarDiv.appendChild(numberInput);
+
+        strokeWidthPreview = new StrokeWidthPreview();
+        toolbarDiv.appendChild(strokeWidthPreview);
+        strokeWidthPreview.refresh();
+
+        var numberInput = new SlideyNumberInput({
+            onsoftchange: function (e) {
+                strokeWidthPreview.refresh(parseInt(e));
+            },
+            onhardchange: function (e) {
+                wickEditor.settings.setValue('strokeWidth', parseInt(e));
+                wickEditor.guiActionHandler.doAction("changeStrokeWidthOfSelection", {
+                    strokeWidth: wickEditor.settings.strokeWidth
+                });
+                wickEditor.syncInterfaces();
+            },
+            min: 1,
+            max: 10,
+            moveFactor: 0.1,
+            initValue: wickEditor.settings.strokeWidth,
+        });
+        numberInput.className = 'toolbar-number-input';
+        toolbarDiv.appendChild(numberInput);
 
     }
 
@@ -54,13 +105,16 @@ var ToolbarInterface = function (wickEditor) {
 
         // Update color picker preview colors
         colorPickerDivs.forEach(function (colorPicker) {
-            //colorPickerContainer.style.backgroundColor = color;
             colorPicker.style.backgroundColor = wickEditor.settings[colorPicker.wickSettingsVal];
         });
 
+        // Update brush/stroke thickness previews
+        brushSizePreview.refresh();
+        strokeWidthPreview.refresh();
+
     }
 
-    function createToolButtonDiv (toolName) {
+    var ToolButton = function (toolName) {
         var tool = wickEditor.tools[toolName];
 
         var toolDiv = document.createElement('div');
@@ -78,10 +132,10 @@ var ToolbarInterface = function (wickEditor) {
         toolIcon.width = '28';
         toolDiv.appendChild(toolIcon);
 
-        toolbarDiv.appendChild(toolDiv);
+        return toolDiv;
     }
 
-    function createColorPickerDiv (settingsVal, className) {
+    var ColorPicker = function (settingsVal, className) {
         var colorPickerContainer = document.createElement('div');
         colorPickerContainer.className = 'toolbar-color ' + className;
         colorPickerContainer.wickSettingsVal = settingsVal;
@@ -101,8 +155,53 @@ var ToolbarInterface = function (wickEditor) {
             colorPickerContainer.getBoundingClientRect().top)
         }
 
-        colorPickerDivs.push(colorPickerContainer);
-        toolbarDiv.appendChild(colorPickerContainer);
+        return colorPickerContainer;
+    }
+
+    var BrushSizePreview = function () {
+        var brushSizePreviewContainer = document.createElement('div');
+        brushSizePreviewContainer.className = 'toolbar-brush-size-preview-container';
+
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        brushSizePreviewContainer.appendChild(svg);
+
+        var ellipse = document.createElementNS(svg.namespaceURI,'ellipse');
+        svg.appendChild(ellipse);
+        brushSizePreviewContainer.refresh = function (val) {
+            var bbox = brushSizePreviewContainer.getBoundingClientRect();
+            if(!val) val = wickEditor.settings.brushThickness;
+            ellipse.setAttribute('cx', bbox.width/2);
+            ellipse.setAttribute('cy', bbox.height/2);
+            ellipse.setAttribute('rx', val/2);
+            ellipse.setAttribute('ry', val/2);
+            ellipse.setAttribute('fill', wickEditor.settings.fillColor);
+        }
+
+        return brushSizePreviewContainer;
+    }
+
+    var StrokeWidthPreview = function () {
+        var strokeWidthPreviewContainer = document.createElement('div');
+        strokeWidthPreviewContainer.className = 'toolbar-stroke-width-preview-container';
+
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        strokeWidthPreviewContainer.appendChild(svg);
+
+        var line = document.createElementNS(svg.namespaceURI,'line');
+        line.setAttribute('x1', 5);
+        line.setAttribute('y1', 7);
+        line.setAttribute('y2', 7);
+        line.setAttribute('x2', 30);
+        line.setAttribute('stroke-linecap', 'round');
+        svg.appendChild(line);
+
+        strokeWidthPreviewContainer.refresh = function (val) {
+            var bbox = strokeWidthPreviewContainer.getBoundingClientRect();
+            if(!val) val = wickEditor.settings.strokeWidth;
+            line.setAttribute('style', 'stroke:'+wickEditor.settings.strokeColor+'; stroke-width:'+val);
+        }
+
+        return strokeWidthPreviewContainer;
     }
 
 }
