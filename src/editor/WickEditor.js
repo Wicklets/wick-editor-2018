1419adb54dbfe5ab2bdb50769f9762d8ece5c985
Zach Rispoli <zach.rispoli@gmail.com>
2017-12-01 02:27:34 -0500

    update editor version

--- src/editor/WickEditor.js
+++ src/editor/WickEditor.js
@@ -22,7 +22,7 @@ var WickEditor = function () {
     var self = this;
 
     // http://semver.org/
-    self.version = "0.10";
+    self.version = "0.11";
     document.getElementById('wick-editor-version').innerHTML = 'Wick Editor ' + self.version;
     console.log("Wick Editor version " + self.version)
     if(localStorage.wickVersion !== self.version) {
