25f2c62a2a9fb05b17efe58a6b78ba9b14de08e4
Zach Rispoli <zach.rispoli@gmail.com>
2017-12-02 17:29:03 -0500

    new pencil cursor

--- src/editor/Tools.Pencil.js
+++ src/editor/Tools.Pencil.js
@@ -22,27 +22,7 @@ Tools.Pencil = function (wickEditor) {
     var that = this;
 
     this.getCursorImage = function () {
-        return 'default';
-
-        var canvas = document.createElement("canvas");
-        canvas.width = 128;
-        canvas.height = 128;
-        var context = canvas.getContext('2d');
-        var centerX = canvas.width / 2;
-        var centerY = canvas.height / 2;
-        var radius = wickEditor.settings.strokeWidth/2 * wickEditor.canvas.getFabricCanvas().canvas.getZoom();
-
-        context.beginPath();
-        context.arc(centerX, centerY, radius+1, 0, 2 * Math.PI, false);
-        context.fillStyle = invertColor(wickEditor.settings.strokeColor);
-        context.fill();
-
-        context.beginPath();
-        context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-        context.fillStyle = wickEditor.settings.strokeColor;
-        context.fill();
-
-        return 'url(' + canvas.toDataURL() + ') 64 64,default';
+        return 'url(resources/cursors/pencil.png) 0 16,default';
     };
 
     this.getToolbarIcon = function () {
