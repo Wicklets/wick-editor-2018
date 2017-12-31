95c014c4e5834c475aea5ba3a3166561758aad77
Zach Rispoli <zach.rispoli@gmail.com>
2017-12-31 03:29:31 -0500

    fix hotkeys windw

--- src/editor/interfaces/Interfaces.EditorSettings.js
+++ src/editor/interfaces/Interfaces.EditorSettings.js
@@ -30,8 +30,19 @@ var EditorSettings = function (wickEditor) {
         var guiActions = wickEditor.guiActionHandler.guiActions;
         for(key in guiActions) {
             var action = guiActions[key];
-            var hotkeys = action.specialKeys.concat(action.hotkeys);
-            if(hotkeys.length === 0 || !action.title) continue;
+            if(action.hotkeys.length === 0 || !action.title) continue;
+            var hotkeysString = action.hotkeys.map(function (k) {
+                // Remove extra works from key string
+                var splitByKey = k.split('Key')
+                k = splitByKey[splitByKey.length-1]
+                var splitByArrow = k.split('Arrow')
+                k = splitByArrow[splitByArrow.length-1]
+                var splitByDigit = k.split('Digit')
+                k = splitByDigit[splitByDigit.length-1]
+                return k;
+            }).join(' + ');
+            if(action.shiftKey) hotkeysString = 'Shift + ' + hotkeysString;
+            if(action.modifierKey) hotkeysString = 'Control + ' + hotkeysString;
 
             var hotkeyDiv = document.createElement('div');
             hotkeyDiv.className = 'hotkey';
@@ -43,7 +54,7 @@ var EditorSettings = function (wickEditor) {
 
             var hotkeyDesc = document.createElement('div');
             hotkeyDesc.className = 'hotkey-description';
-            hotkeyDesc.innerHTML = hotkeys.join(' + ');
+            hotkeyDesc.innerHTML = hotkeysString;
             hotkeyDiv.appendChild(hotkeyDesc);
 
             hotkeysContainer.appendChild(hotkeyDiv);
